63d749ddf4b1da5d378464adb325def1c1bccde1
marviN310 <92372247+marviN310@users.noreply.github.com>
2023-05-24 22:43:07 +0200

    streamlit application

---
+++ streamlit_app.py
@@ -0,0 +1,209 @@
+import xgboost as xgb
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Loading up the Regression model we created
+model = xgb.XGBRegressor()
+model.load_model('xgb_model.json')
+
+# Caching the model for faster loading
+
+
+# Feature column expected by model
+features = model.get_booster().feature_names
+
+
+# Converting string output to one-hot encoding for model input
+
+
+def encoding_category(category, list):
+    indices = [i for i, element in enumerate(list) if category in element]
+
+    one_hot = np.eye(len(list))[indices]
+
+    return one_hot.tolist()[0]
+
+
+# Categories (order important)
+
+genres_categories = ['Action',
+                     'Adventure',
+                     'Animation',
+                     'Biography',
+                     'Comedy',
+                     'Crime',
+                     'Documentary',
+                     'Drama',
+                     'Family',
+                     'Fantasy',
+                     'History',
+                     'Horror',
+                     'Music',
+                     'Musical',
+                     'Mystery',
+                     'Romance',
+                     'Sci-Fi',
+                     'Sport',
+                     'Thriller',
+                     'War',
+                     'Western']
+
+company_categories = ['Distribution company_20th Century Studios',
+                      'Distribution company_A24',
+                      'Distribution company_Amazon Studios',
+                      'Distribution company_Bleecker Street',
+                      'Distribution company_CBS Films',
+                      'Distribution company_Entertainment One',
+                      'Distribution company_FilmDistrict',
+                      'Distribution company_Focus Features',
+                      'Distribution company_Freestyle Releasing',
+                      'Distribution company_IFC Films',
+                      'Distribution company_Lionsgate',
+                      'Distribution company_Magnet Releasing',
+                      'Distribution company_Magnolia Pictures',
+                      'Distribution company_Neon',
+                      'Distribution company_Netflix',
+                      'Distribution company_Open Road Films',
+                      'Distribution company_Other',
+                      'Distribution company_Paramount Pictures',
+                      'Distribution company_Pure Flix Entertainment',
+                      'Distribution company_Relativity Media',
+                      'Distribution company_Roadside Attractions',
+                      'Distribution company_STXfilms',
+                      'Distribution company_Saban Films',
+                      'Distribution company_Searchlight Pictures',
+                      'Distribution company_Sony Pictures Classics',
+                      'Distribution company_Sony Pictures Releasing',
+                      'Distribution company_StudioCanal',
+                      'Distribution company_Summit Entertainment',
+                      'Distribution company_The Weinstein Company',
+                      'Distribution company_United Artists Releasing',
+                      'Distribution company_Universal Pictures',
+                      'Distribution company_Vertical Entertainment',
+                      'Distribution company_Walt Disney Studios',
+                      'Distribution company_Warner Bros. Pictures']
+
+months = ['Release month_April',
+          'Release month_August',
+          'Release month_December',
+          'Release month_February',
+          'Release month_January',
+          'Release month_July',
+          'Release month_June',
+          'Release month_March',
+          'Release month_May',
+          'Release month_November',
+          'Release month_October',
+          'Release month_September']
+
+ratings = ['Age Rating_(Banned)',
+           'Age Rating_0',
+           'Age Rating_12',
+           'Age Rating_16',
+           'Age Rating_16+',
+           'Age Rating_18',
+           'Age Rating_6',
+           'Age Rating_G',
+           'Age Rating_Not Rated',
+           'Age Rating_PG',
+           'Age Rating_PG-13',
+           'Age Rating_R',
+           'Age Rating_TV-14',
+           'Age Rating_Unrated']
+
+
+def predict(runningtime, budget, company, month, genres, rating):
+    genres_one_hot = encoding_category(genres, genres_categories)
+    company_one_hot = encoding_category(company, company_categories)
+    month_one_hot = encoding_category(month, months)
+    ratings_one_hot = encoding_category(rating, ratings)
+
+    all_list = [runningtime] + [budget] + genres_one_hot + company_one_hot + month_one_hot + ratings_one_hot
+    prediction = model.predict(pd.DataFrame([all_list], columns=features))
+    return prediction
+
+
+st.title('Box Office Predictor')
+
+runningtime = st.slider('Running time in minutes', 0, 250, 25)
+
+# Streamlit number
+budget = st.slider('Budget in Mio $', 0, 500, 20)
+
+company = st.selectbox('Distribution company:', ['20th Century Studios',
+                                                 'A24',
+                                                 'Amazon Studios',
+                                                 'Bleecker Street',
+                                                 'CBS Films',
+                                                 'Entertainment One',
+                                                 'FilmDistrict',
+                                                 'Focus Features',
+                                                 'Freestyle Releasing',
+                                                 'IFC Films',
+                                                 'Lionsgate',
+                                                 'Magnet Releasing',
+                                                 'Magnolia Pictures',
+                                                 'Neon',
+                                                 'Netflix',
+                                                 'Open Road Films',
+                                                 'Paramount Pictures',
+                                                 'Pure Flix Entertainment',
+                                                 'Relativity Media',
+                                                 'Roadside Attractions',
+                                                 'STXfilms',
+                                                 'Saban Films',
+                                                 'Searchlight Pictures',
+                                                 'Sony Pictures Classics',
+                                                 'Sony Pictures Releasing',
+                                                 'StudioCanal',
+                                                 'Summit Entertainment',
+                                                 'The Weinstein Company',
+                                                 'United Artists Releasing',
+                                                 'Universal Pictures',
+                                                 'Vertical Entertainment',
+                                                 'Walt Disney Studios',
+                                                 'Warner Bros. Pictures', 'Other'])
+
+month = st.selectbox('Release month:', ['April',
+                                        'August',
+                                        'December',
+                                        'February',
+                                        'January',
+                                        'July',
+                                        'June',
+                                        'March',
+                                        'May',
+                                        'November',
+                                        'October',
+                                        'September'])
+
+genres = st.selectbox("Genres:", ['Action',
+                                  'Adventure',
+                                  'Animation',
+                                  'Biography',
+                                  'Comedy',
+                                  'Crime',
+                                  'Documentary',
+                                  'Drama',
+                                  'Family',
+                                  'Fantasy',
+                                  'History',
+                                  'Horror',
+                                  'Music',
+                                  'Musical',
+                                  'Mystery',
+                                  'Romance',
+                                  'Sci-Fi',
+                                  'Sport',
+                                  'Thriller',
+                                  'War',
+                                  'Western'])
+
+rating = st.selectbox("Age Rating:", ["0","6","G","PG","12","PG-13"
+                                       "TV-14","16","16+","R","18","Banned","Not Rated","Unrated"])
+
+
+if st.button('Predict Box Office'):
+    price = predict(runningtime, budget, company, month, genres, rating)
+    st.success(f'The predicted Box Office is ${price[0]} Mio USD')
