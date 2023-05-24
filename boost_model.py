f72ad5a317d65832419d3a6592377aa447ada811
marviN310 <92372247+marviN310@users.noreply.github.com>
2023-05-24 17:22:35 +0200

    Add files via upload

---
+++ boost_model.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from xgboost import XGBRegressor
+
+
+# Import preprocessed data
+movie_data_new = pd.read_csv("full_movie_data_processed.csv")
+
+
+
+#Feature-Target split
+movie_features = movie_data_new.drop(["Box office","Title"],axis=1)
+target = movie_data_new["Box office"]
+
+# Use simple XGBoost model for prediction
+
+xgb_cl = XGBRegressor()
+
+xgb_cl.fit(movie_features, target)
+
+xgb_cl.save_model('xgb_model.json')
