65b5aaebc47951f3564a0a5c014fe0737dedbe17
marviN310 <92372247+marviN310@users.noreply.github.com>
2023-06-05 22:03:37 +0200

    Update boost_model.py

--- boost_model.py
+++ boost_model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from xgboost import XGBRegressor
-
+from sklearn.model_selection import GridSearchCV
 
 # Import preprocessed data
 movie_data_new = pd.read_csv("full_movie_data_processed.csv")
@@ -11,11 +11,24 @@ movie_data_new = pd.read_csv("full_movie_data_processed.csv")
 movie_features = movie_data_new.drop(["Box office","Title"],axis=1)
 target = movie_data_new["Box office"]
 
-# Use simple XGBoost model for prediction
+# Using GridSearch for hyperparameter tuning on XGBoost model
 
 xgb_cl = XGBRegressor()
 
-xgb_cl.fit(movie_features, target)
+param_grid = {
+    "max_depth": [3, 4, 5, 7],
+    "learning_rate": np.arange(0.1, 0.6, 0.1),
+    "n_estimators": [50, 60, 70, 80, 90, 100],
+    "reg_lambda": [0, 1, 10],
+    "subsample": [0.8, 1],
+}
 
-# Save model
-xgb_cl.save_model('xgb_model.json')
+grid_cv = GridSearchCV(xgb_cl, param_grid, scoring="neg_mean_absolute_error")
+
+#Train model
+
+grid_cv.fit(movie_features, target)
+
+#Save model
+best_model = grid_cv.best_estimator_
+best_model.save_model('xgb_model_cv.json')
